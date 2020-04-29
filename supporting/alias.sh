fcf7af58dfb770fd3128f9d62abe7c51bc925f46
rg772 <38052821+rg772@users.noreply.github.com>
2020-04-29 10:25:07 -0500

    Update alias.sh

--- supporting/alias.sh
+++ supporting/alias.sh
@@ -2,8 +2,8 @@
 alias wp='wp --allow-root '
 alias ll='ls -lah '
 
-
-function install_helpers {
+# Some stuff I need to upstart in a session
+function upsie {
   apt update -y
   apt install vim less git -y
 }
